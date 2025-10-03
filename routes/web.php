406a6470ff050a9457204e38d00fc88afcb1a2c4
K3rllon <francisco.silva9654@aluno.ce.gov.br>
2025-10-03 08:53:38 -0300

    Criação da primeira route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategorysController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categorys', [CategorysController::class, "index"]);
