60bf6f13b6fe2fec71a50ddb69bdf3f0718e02dc
K3rllon <francisco.silva9654@aluno.ce.gov.br>
2025-10-03 21:07:12 -0300

    Criação da rota para edição

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Route::get('/categorys', [CategorysController::class, "index"])->name("categorys.index");
 Route::get('/categorys/create', [CategorysController::class, "create"])->name("categorys.create");
 Route::post('/categorys', [CategorysController::class, "store"])->name("categorys.store");
+Route::post('/categorys/{category}/edit', [CategorysController::class, "edit"])->name("categorys.edit");
